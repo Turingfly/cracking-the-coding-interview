a5105f01565367bb0f8ed0cb227bc1357828d39b
turingfly <chengfei@udel.edu>
2017-06-22 22:52:08 -0400

    Trees and Graphs

---
+++ src/chapter04TreesAndGraphs/CheckBalancedTree.java
@@ -0,0 +1,67 @@
+package chapter04TreesAndGraphs;
+
+/**
+ * 
+ * @author chengfeili 
+ * Jun 22, 2017 7:42:31 PM
+ * 
+ *         Problem: Implement a function to check if a binary tree is balanced.
+ *         For the purposes of this question, a balanced tree is defined to be a
+ *         tree such that the heights of the two subtrees of any node never
+ *         differ by more than one.
+ *
+ *         Solution:
+ *
+ */
+public class CheckBalancedTree {
+	// method 1: O(NlogN)
+	private int getHeight(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+	}
+
+	public boolean isBalanced(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+		if (Math.abs(getHeight(root.left) - getHeight(root.right)) > 1) {
+			return false;
+		} else {
+			return isBalanced(root.left) && isBalanced(root.right);
+		}
+	}
+
+	// method 2: O(N)
+	class ResultType {
+		boolean isBalanced;
+		int maxDepth;
+
+		public ResultType(boolean isBalanced, int maxDepth) {
+			this.isBalanced = isBalanced;
+			this.maxDepth = maxDepth;
+		}
+	}
+
+	public boolean isBalanced2(TreeNode root) {
+		return helper(root).isBalanced;
+	}
+
+	private ResultType helper(TreeNode root) {
+		if (root == null) {
+			return new ResultType(true, 0);
+		}
+		ResultType left = helper(root.left);
+		ResultType right = helper(root.right);
+		// subtree is not balanced
+		if (!left.isBalanced || !right.isBalanced) {
+			return new ResultType(false, -1);
+		}
+		// root is not balanced
+		if (Math.abs(left.maxDepth - right.maxDepth) > 1) {
+			return new ResultType(false, -1);
+		}
+		return new ResultType(true, Math.max(left.maxDepth, right.maxDepth) + 1);
+	}
+}
---
+++ src/chapter04TreesAndGraphs/ValidateBST.java
@@ -0,0 +1,104 @@
+package chapter04TreesAndGraphs;
+
+/**
+ * 
+ * @author chengfeili 
+ * Jun 22, 2017 7:52:41 PM
+ * 
+ *         Problem: Implement a function to check if a binary tree is a binary
+ *         search tree.
+ *
+ *         Solution: The first leverages the in-order traversal, and the second
+ *         builds off the property that left <= current < right
+ *         
+ *         Time Complexity: O(N)
+ *         
+ *         Space Complexity: O(H). H is the height of the tree.
+ *
+ */
+public class ValidateBST {
+	// method 1: Assume that the tree cannot have duplicate values. In-Order
+	// Traversal
+	class WrapInt {
+		public int val;
+
+		public WrapInt(int val) {
+			this.val = val;
+		}
+	}
+
+	WrapInt last_printed = null;
+
+	public boolean isValidBST(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+		// check left
+		if (!isValidBST(root.left)) {
+			return false;
+		}
+		// check current
+		if ((last_printed != null) && last_printed.val >= root.val) {
+			return false;
+		}
+		last_printed = new WrapInt(root.val);
+		// check right
+		if (!isValidBST(root.right)) {
+			return false;
+		}
+		return true;
+	}
+
+	// method 2: left <= current < right; The Min / Max Solution
+	public boolean isValidBST2(TreeNode root) {
+		return check(root, null, null);
+	}
+	
+	public boolean check(TreeNode root, Integer min, Integer max) {
+		if (root == null) {
+			return true;
+		}
+		// When we branch right, min gets updated.
+		// When we branch left, max gets updated.
+		if ((min != null && root.val <= min) || (max != null && root.val > max)) {
+			return false;
+		}
+		if (!check(root.left, min, root.val) || !check(root.right, root.val, max)) {
+			return false;
+		}
+		return true;
+	}
+
+	// method 2.5: left <= current < right; The Min / Max Solution
+	class ResultType {
+		int max;
+		int min;
+		boolean is_bst;
+
+		public ResultType(int max, int min, boolean is_bst) {
+			this.max = max;
+			this.min = min;
+			this.is_bst = is_bst;
+		}
+	}
+
+	public boolean isValidBST25(TreeNode root) {
+		ResultType res = helper(root);
+		return res.is_bst;
+	}
+
+	private ResultType helper(TreeNode root) {
+		if (root == null) {
+			return new ResultType(Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+		}
+		ResultType left = helper(root.left);
+		ResultType right = helper(root.right);
+		if (!left.is_bst || !right.is_bst) {
+			return new ResultType(0, 0, false);
+		}
+		if (root.left != null && left.max > root.val || root.right != null && right.min <= root.val) {
+			return new ResultType(0, 0, false);
+		}
+		return new ResultType(Math.max(root.val, right.max), Math.min(root.val, left.min), true);
+	}
+}
