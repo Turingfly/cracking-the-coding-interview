956cd86c8e3899f66ac7f907da329196cf37e859
turingfly <chengfei@udel.edu>
2017-07-17 23:07:53 -0400

    Trees and Graphs

---
+++ src/chapter04TreesAndGraphs/PathsWithSum.java
@@ -0,0 +1,106 @@
+package chapter04TreesAndGraphs;
+
+import java.util.HashMap;
+
+/**
+ * 
+ * @author chengfeili 
+ * Jul 17, 2017 9:04:58 PM
+ * 
+ * Problem: You are given a binary tree in which each node contains an integer
+ * value (which might be positive or negative). Design an algorithm to count the
+ * number of paths that sum to a given value. The path does not need to start or
+ * end at the root or a leaf, but it must go downwards (traveling only from
+ * parent nodes to child nodes).
+ *
+ */
+
+public class PathsWithSum {
+
+	/**
+	 * Method 1: Brute force Traverse to each node. At each node, try all
+	 * downwards, tracking the sum. Increase once hit the target sum.
+	 * 
+	 * Time Complexity: O(NlogN) in a balanced tree. O(N2) in an unbalanced
+	 * tree. Node at depth d will be touched by d nodes above it.
+	 */
+	public int countPathsWithSum(TreeNode root, int target) {
+		if (root == null) {
+			return 0;
+		}
+		// count paths with sum starting from the root
+		int pathsFromRoot = countPathsWithSumFromNode(root, target, 0);
+		int pathsOnLeft = countPathsWithSum(root.left, target);
+		int pathsOnRight = countPathsWithSum(root.right, target);
+		return pathsFromRoot + pathsOnLeft + pathsOnRight;
+	}
+
+	// returns the number of paths with this sum starting from this node
+	private int countPathsWithSumFromNode(TreeNode root, int target, int curSum) {
+		int totalPaths = 0;
+		if (root == null) {
+			return totalPaths;
+		}
+		curSum += root.val;
+		if (curSum == target) {
+			totalPaths++;
+		}
+		totalPaths += countPathsWithSumFromNode(root.left, target, curSum);
+		totalPaths += countPathsWithSumFromNode(root.right, target, curSum);
+		return totalPaths;
+	}
+
+	/**
+	 * Method 2: Traverse through the tree using DFS. As we visit each node:
+	 * 
+	 * 1. Track its runningSum.We'll take this in as a parameter and immediately
+	 * increment it by node.val;
+	 * 
+	 * 2. Look up runningSum - targetSum in the hash table. The value there
+	 * indicates the total number. Set totalPaths to this value;
+	 * 
+	 * 3. If runningSum == targetSum in the hash table, then there's one
+	 * additional path that starts at the root. Increment totalPaths.
+	 * 
+	 * 4. Update runningSum to the hash table.
+	 * 
+	 * 5. Recurse left and right, counting the number of paths with sum
+	 * targetSum.
+	 * 
+	 * 6. After we're done recurring left and right, decrement the value of
+	 * runningSum in the hash table. This is essentially backing out of our
+	 * work; it reverses the changes to the hash table so that other nodes don't
+	 * use it.
+	 * 
+	 */
+	public int countPathsWithSum2(TreeNode root, int targetSum) {
+		return countPathsWithSum2(root, targetSum, 0, new HashMap<Integer, Integer>());
+	}
+
+	private int countPathsWithSum2(TreeNode node, int targetSum, int runningSum, HashMap<Integer, Integer> map) {
+		if (node == null) {
+			return 0;
+		}
+		runningSum += node.val;
+		int sum = runningSum - targetSum;
+		int totalPaths = map.getOrDefault(sum, 0);
+		if (runningSum == targetSum) {
+			totalPaths++;
+		}
+		incrementHash(map, runningSum, 1);
+		totalPaths += countPathsWithSum2(node.left, targetSum, runningSum, map);
+		totalPaths += countPathsWithSum2(node.right, targetSum, runningSum, map);
+		incrementHash(map, runningSum, -1);
+		return totalPaths;
+	}
+
+	private void incrementHash(HashMap<Integer, Integer> map, int key, int delta) {
+		int newCount = map.getOrDefault(key, 0) + delta;
+		if (newCount == 0) {
+			// remove when zero to reduce space usage
+			map.remove(key);
+		} else {
+			map.put(key, newCount);
+		}
+	}
+}
---
+++ src/chapter04TreesAndGraphs/RandomNode.java
@@ -0,0 +1,98 @@
+package chapter04TreesAndGraphs;
+
+import java.util.Random;
+
+/**
+ * 
+ * @author chengfeili
+ * Jul 17, 2017 8:48:39 PM
+ * 
+ * Problem: You are implementing a binary search tree class from scratch, which,
+ * in addition to insert, find, and delete, has a method getRandomNode() which
+ * returns a random node from the tree. All nodes should be equally likely to be
+ * chosen. Design and implement an algorithm for getRandomNode, and explain how
+ * you would implement the rest of the methods.
+ * 
+ * Time Complexity: O(logN), where N is the number of nodes.
+ *
+ */
+public class RandomNode {
+	TreeNode root = null;
+
+	public void insertInOrder(int val) {
+		if (root == null) {
+			root = new TreeNode(val);
+		} else {
+			root.insertInOrder(val);
+		}
+	}
+
+	public int size() {
+		return root == null ? 0 : root.getSize();
+	}
+
+	public TreeNode getRandomNode() {
+		if (root == null) {
+			return null;
+		}
+		Random random = new Random();
+		int i = random.nextInt(size());
+		return root.getIthNode(i);
+	}
+
+	class TreeNode {
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		private int size = 0;
+
+		public TreeNode(int val) {
+			val = this.val;
+			size = 1;
+		}
+
+		public void insertInOrder(int val) {
+			if (this.val <= val) {
+				if (left == null) {
+					left = new TreeNode(this.val);
+				} else {
+					left.insertInOrder(this.val);
+				}
+			} else {
+				if (right == null) {
+					right = new TreeNode(this.val);
+				} else {
+					right.insertInOrder(this.val);
+				}
+			}
+			size++;
+		}
+
+		public int getSize() {
+			return size;
+		}
+
+		public TreeNode find(int val) {
+			if (this.val == val) {
+				return this;
+			} else if (this.val <= val) {
+				return left != null ? left.find(this.val) : null;
+			} else if (this.val > val) {
+				return right != null ? right.find(this.val) : null;
+			}
+			return null;
+		}
+
+		public TreeNode getIthNode(int i) {
+			int leftSize = left == null ? 0 : left.getSize();
+			if (i < leftSize) {
+				return left.getIthNode(i);
+			} else if (i == leftSize) {
+				return this;
+			} else {
+				return right.getIthNode(i - (leftSize + 1));
+			}
+		}
+	}
+
+}
