e946af5e6c80de44ccf3f637dbb1b8ad54560cb6
turingfly <chengfei@udel.edu>
2017-07-18 22:28:27 -0400

    Bit Manipulation

---
+++ src/chapter05BitManipulation/BinaryToString.java
@@ -0,0 +1,59 @@
+package chapter05BitManipulation;
+
+/**
+ * 
+ * @author chengfeili
+ * Jul 18, 2017 10:00:07 AM
+ * 
+ * Problem: Given a real number between 0 and 1(e.g., 0.72) that is passed in as
+ * a double, print the binary representation. If the number cannot be
+ * represented accurately in binary with at most 32 characters, print "ERROR".
+ *
+ */
+public class BinaryToString {
+	// Method 1: multiplying the number by two and comparing it to 1.
+	public String getBinary1(double num) {
+		if (num >= 1 || num <= 0) {
+			return "ERROR";
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append(".");
+		while (num > 0) {
+			if (sb.length() >= 32) {
+				return "ERROR";
+			}
+			double update = num * 2;
+			if (update >= 1) {
+				sb.append("1");
+				num = update - 1;
+			} else {
+				sb.append("0");
+				num = update;
+			}
+		}
+		return sb.toString();
+	}
+	
+	// Method 2: compare the number to 0.5, 0.25....
+	public String getBinary2(double num) {
+		if (num >= 1 || num <= 0) {
+			return "ERROR";
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append(".");
+		double frac = 0.5;
+		while (num > 0) {
+			if (sb.length() >= 32) {
+				return "ERROR";
+			}
+			if (num >= frac) {
+				sb.append("1");
+				num -= frac;
+			} else {
+				sb.append("0");
+			}
+			frac /= 2;
+		}
+		return sb.toString();
+	}
+}
---
+++ src/chapter05BitManipulation/Intro.java
@@ -0,0 +1,64 @@
+package chapter05BitManipulation;
+
+/**
+ * 
+ * @author chengfeili
+ * Jul 18, 2017 9:38:09 AM
+ * 
+ * Bit Facts and Tricks
+ * x ^ 0s = x       x & 0s = 0		x | 0s = x
+ * x ^ 1s = ~x		x & 1s = x		x | 1s = 1s
+ * x ^ x = 0		x & x = x 		x | x = x
+ * 
+ * The binary representation of -K as a N-bit number is concat(1, 2(N-1) -k).
+ * 7: 0111;  -1: 1111
+ * 
+ */
+public class Intro {
+	// arithmetic right shift; x =-93242, count = 40; return -1
+	public int repeatedArithmeticShift(int x, int count) {
+		for (int i = 0; i < count; i++) {
+			x >>= 1;
+		}
+		return x;
+	}
+
+	// logic right shift; x =-93242, count = 40; return 0
+	public int repeatedLogicalShift(int x, int count) {
+		for (int i = 0; i < count; i++) {
+			x >>>= 1;
+		}
+		return x;
+	}
+
+	public int getBit(int num, int i) {
+		return ((num & (1 << i)) != 0) ? 1 : 0;
+	}
+
+	public int setBit(int num, int i) {
+		return num | (1 << i);
+	}
+
+	public int clearBit(int num, int i) {
+		int mask = ~(1 << i);
+		return num & mask;
+	}
+
+	// clear all bits from the most significant bit through i(inclusive)
+	public int clearBitsMSBthroughI(int num, int i) {
+		int mask = (1 << i) - 1;
+		return num & mask;
+	}
+
+	// clear all bits from i through 0 (inclusive)
+	public int clearBitsIthrough0(int num, int i) {
+		int mask = (-1 << (i + 1));
+		return num & mask;
+	}
+
+	public int updateBit(int num, int i, boolean bitIs1) {
+		int val = bitIs1 ? 1 : 0;
+		int mask = ~(1 << i);
+		return (num & mask) | (val << i);
+	}
+}
